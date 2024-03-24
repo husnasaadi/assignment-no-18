e60f8edcf19fc98968cc3cb06cd4c65067d63d84
sadia saadi <saadiasaadi944@gmail.com>
2024-03-24 08:35:39 +0500

    assignment no 18

---
+++ app.js
@@ -0,0 +1,69 @@
+// // // chap 21-25
+
+// // // q;1
+// // // var allLower=userinput.user.toLowercase();
+
+// // // q;2
+// // x = x.toLowerCase();
+
+// // // q;3
+// Y=Y.touppercase();
+
+// q;4
+// var varible="hello world";
+// var resut=varible.toLowerCase();
+
+// q;5
+// var array=["hello", "world"];
+// var result=array.tolowercase();
+
+// q;6
+// var varible="helllo world";
+// alert(varible.toUpperCase());
+
+// q;7
+// var cityName=kArAChi;
+// var name=cityName[0].touppercase()+cityName.slice(1).tolowercase();
+// alert(name);
+
+
+
+// q;1
+// var samewords="captain";
+// var assign=samewords.slice(1,3);
+// alert(assign);
+
+// q;2
+// var myString="hello worl";
+// var myStringlength=myString.length;
+// alert(myStringlength)
+
+// q;6
+// var text="to be or not to be";
+// var indx=text.indexOf("be");
+// alert(indx)
+
+// q;7
+// var text="to be or not to be";
+// var indx=text.lastIndexOf("be");
+// alert(indx)
+
+// q;8
+// var text = "let's go, go, go!";
+// var indx = text.lastIndexOf("go");
+// alert(indx);
+
+// q;9
+// var myString = "Hello, World!";
+// var indexNum = 4;
+
+// if (myString.charAt(indexNum) !== "") {
+    // console.log("Segment exists at index " + indexNum + ".");
+// } else {
+    // console.log("No segment exists at index " + indexNum + ".");
+// }
+
+// q;10
+// var alppha="abcde";
+// var indexNum=alppha.charAt(2);
+// alert(indexNum);
